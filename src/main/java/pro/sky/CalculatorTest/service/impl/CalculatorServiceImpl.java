bf08593aa4a3d7e24fa6e18d181279f6281ac590
Aleksei <alexejsedow@gmail.com>
2025-01-04 22:25:23 +0300

    start

---
+++ src/main/java/pro/sky/CalculatorTest/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,28 @@
+package pro.sky.CalculatorTest.service.impl;
+
+import org.springframework.stereotype.Service;
+import pro.sky.CalculatorTest.service.api.CalculatorService;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    @Override
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
